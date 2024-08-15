2c69ba47e7ded79bd6a9327fb2e91581050ccd42
Skole <600847@stud.hvl.no>
2024-08-15 17:55:01 +0200

    feat:add-nav-mobile

--- components/PageTransition.jsx
+++ components/PageTransition.jsx
@@ -11,7 +11,7 @@ const PageTransition = ({ children }) => {
           initial={{ opacity: 1 }}
           animate={{
             opacity: 0,
-            transition: { delay: 0.4, duration: 0.4, ease: "easeInOut" },
+            transition: { delay: 0.1, duration: 0.4, ease: "easeInOut" },
           }}
           className="h-screen w-screen fixed bg-primary top-0 pointer-events-none"
         ></motion.div>
