e8d2aa84d5685944517e863bf7dcf1ad17ffa565
Skole <600847@stud.hvl.no>
2024-08-15 16:07:28 +0200

    feat:add-nav-desktop

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,14 +1,32 @@
 import Link from "next/link";
 import { Button } from "./ui/button";
+
+//components
+import Nav from "./Nav";
+
 const Header = () => {
   return (
     <header className="py-8 xl:py-12 text-white">
-      <div className="container mx-auto">
+      <div className="container mx-auto flex justify-between items-center">
         <Link href="/">
           <h1 className="text-4xl font-semibold">
             August <span className="text-accent">.</span>
           </h1>
         </Link>
+
+        {/* desktop nav */}
+        <div className="hidden xl:flex items-center gap-8">
+          <Nav />
+          <Link href="/contact">
+            <Button className="bg-accent hover:bg-accent-hover">
+              {" "}
+              Ansatt meg
+            </Button>
+          </Link>
+        </div>
+
+        {/* mobile nav */}
+        <div className="xl:hidden"> mobile nav </div>
       </div>
     </header>
   );
