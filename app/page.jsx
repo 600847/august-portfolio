fc42ae0594852683455f014e5f69dc0716aa02a0
Skole <600847@stud.hvl.no>
2024-08-15 18:24:09 +0200

    feat:add-introtext

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,41 @@
-export default function Home() {
-  return <main>Homepage</main>;
-}
+import { Button } from "@/components/ui/button";
+import { FiDownload } from "react-icons/fi";
+
+const Home = () => {
+  return (
+    <section className="h-full">
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+          {/* text */}
+          <div className="text-center xl:text-left">
+            <span className="text-xl">Software Developer</span>
+            <h1 className="h1 mb-6">
+              Hello, I'm <br />
+              <span className="text-accent">August Helle </span>
+            </h1>
+            <p className="max-w[500px] mb-9 text-white/80">
+              I excel at being extremly good at coding
+            </p>
+
+            {/* btn and socials */}
+            <div>
+              <Button
+                variant="outline"
+                size="lg"
+                className="uppercase flex items-center gap-2"
+              >
+                <span>Download CV</span>
+                <FiDownload className="text-xl" />
+              </Button>
+            </div>
+          </div>
+
+          {/* photo */}
+          <div>Photo</div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
